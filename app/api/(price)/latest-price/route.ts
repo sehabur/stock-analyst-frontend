e73ce937548d0281291f55a4e1b00fd3f8ba70a4
sehabur <sehabur@gmail.com>
2024-04-24 20:21:38 +0600

    bug fix

--- app/api/(price)/latest-price/route.ts
+++ app/api/(price)/latest-price/route.ts
@@ -2,7 +2,7 @@ import { NextResponse, NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
   const res = await fetch(`${process.env.BACKEND_URL}/api/prices/latestPrice`, {
-    next: { revalidate: 60 },
+    next: { revalidate: 0 },
   });
 
   const data = await res.json();
